a41a9e35d3deaf2e356771a3fe5e8bd681f80033
chengp97 <1978438017@qq.com>
2023-11-03 09:15:41 +0800

    11-03-1dev

--- a.js
+++ a.js
@@ -1,28 +1,28 @@
-const a = 1
+const a = 1;
 
-const b =2
+const b = 2;
 
-const c = 3
+const c = 3;
 
-const dm = 4
+const dm = 4;
 
-const df = 4
+const df = 4;
 
-const em = 5
-const ef = 5
+const em = 5;
+const ef = 5;
 
-const fd = 6
+const fd = 6;
 
-const gd = 7
+const gd = 7;
 
 console.log("Git is a free software ...");
 
 console.log("dev2");
-console.log("dev")
+console.log("dev");
 
-console.log("提交一次")
+console.log("提交一次");
 console.log("提交二次");
-console.log("dev2第一次提交")
+console.log("dev2第一次提交");
 console.log("dev2第二次提交");
 console.log("提交三次");
 console.log("提交四次");
